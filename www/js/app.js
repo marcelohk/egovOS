cbb709605b44e9e3941eb43d9d0ced5cc37930cb
mar150 <mar150@hotmail.com>
2015-09-27 20:31:25 -0300

    funcionando login

--- www/js/app.js
+++ www/js/app.js
@@ -1,73 +1,86 @@
-// Ionic Starter App
+// Ionic Parse Starter App
 
 // angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// 'ionicParseApp' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-// 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers'])
+// 'ionicParseApp.controllers' is found in controllers.js
+angular.module('ionicParseApp',
+        [ 'ionic', 'ionicParseApp.controllers' ]
+    )
+    .config(function($stateProvider, $urlRouterProvider) {
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
+        // Ionic uses AngularUI Router which uses the concept of states
+        // Learn more here: https://github.com/angular-ui/ui-router
+        // Set up the various states which the app can be in.
+        // Each state's controller can be found in controllers.js
+        $stateProvider
 
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
-})
+            // setup an abstract state for the tabs directive
+            .state('welcome', {
+                url: '/welcome?clear',
+                templateUrl: 'templates/welcome.html',
+                controller: 'WelcomeController'
+            })
 
-.config(function($stateProvider, $urlRouterProvider) {
-  $stateProvider
+            .state('app', {
+                url: '/app?clear',
+                abstract: true,
+                templateUrl: 'templates/menu.html',
+                controller: 'AppController'
+            })
 
-    .state('app', {
-    url: '/app',
-    abstract: true,
-    templateUrl: 'templates/menu.html',
-    controller: 'AppCtrl'
-  })
+            .state('app.home', {
+                url: '/home',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'templates/home.html',
+                        controller: 'HomeController'
+                    }
+                }
+            })
 
-  .state('app.search', {
-    url: '/search',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/search.html'
-      }
-    }
-  })
+            .state('app.login', {
+                url: '/login',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'templates/login.html',
+                        controller: 'LoginController'
+                    }
+                }
+            })
 
-  .state('app.browse', {
-      url: '/browse',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/browse.html'
-        }
-      }
+            .state('app.forgot', {
+                url: '/forgot',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'templates/forgotPassword.html',
+                        controller: 'ForgotPasswordController'
+                    }
+                }
+            })
+
+            .state('app.register', {
+                url: '/register',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'templates/register.html',
+                        controller: 'RegisterController'
+                    }
+                }
+            });
+
+        $urlRouterProvider.otherwise('/welcome');
     })
-    .state('app.playlists', {
-      url: '/playlists',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/playlists.html',
-          controller: 'PlaylistsCtrl'
-        }
-      }
-    })
+    .run(function ($state, $rootScope) {
+        Parse.initialize('Y8gJDJspuwm5nYmqlefEt67Q4Ij5H8k25efoZW7g', 'hUXNYOIiIK8LelyWu9UMXIrSqQnufDvqxpzagDTT');
+        var currentUser = Parse.User.current();
+        $rootScope.user = null;
+        $rootScope.isLoggedIn = false;
 
-  .state('app.single', {
-    url: '/playlists/:playlistId',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/playlist.html',
-        controller: 'PlaylistCtrl'
-      }
-    }
-  });
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
-});
+        if (currentUser) {
+            $rootScope.user = currentUser;
+            $rootScope.isLoggedIn = true;
+            $state.go('app.home');
+        }
+    });
+
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,56 +1,204 @@
-angular.module('starter.controllers', [])
+angular.module('ionicParseApp.controllers', [])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+.controller('AppController', function($scope, $state, $rootScope, $ionicHistory, $stateParams) {
+    if ($stateParams.clear) {
+        $ionicHistory.clearHistory();
+        $ionicHistory.clearCache();
+    }
 
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
-
-  // Form data for the login modal
-  $scope.loginData = {};
-
-  // Create the login modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/login.html', {
-    scope: $scope
-  }).then(function(modal) {
-    $scope.modal = modal;
-  });
-
-  // Triggered in the login modal to close it
-  $scope.closeLogin = function() {
-    $scope.modal.hide();
-  };
-
-  // Open the login modal
-  $scope.login = function() {
-    $scope.modal.show();
-  };
-
-  // Perform the login action when the user submits the login form
-  $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
-
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
-    $timeout(function() {
-      $scope.closeLogin();
-    }, 1000);
-  };
+    $scope.logout = function() {
+        Parse.User.logOut();
+        $rootScope.user = null;
+        $rootScope.isLoggedIn = false;
+        $state.go('welcome', {
+            clear: true
+        });
+    };
 })
 
-.controller('PlaylistsCtrl', function($scope) {
-  $scope.playlists = [
-    { title: 'Reggae', id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
-  ];
+.controller('WelcomeController', function($scope, $state, $rootScope, $ionicHistory, $stateParams) {
+    if ($stateParams.clear) {
+        $ionicHistory.clearHistory();
+        $ionicHistory.clearCache();
+    }
+
+    $scope.login = function() {
+        $state.go('app.login');
+    };
+
+    $scope.signUp = function() {
+        $state.go('app.register');
+    };
+
+    if ($rootScope.isLoggedIn) {
+        $state.go('app.home');
+    }
 })
 
-.controller('PlaylistCtrl', function($scope, $stateParams) {
+.controller('HomeController', function($scope, $state, $rootScope) {
+
+    if (!$rootScope.isLoggedIn) {
+        $state.go('welcome');
+    }
+})
+
+.controller('LoginController', function($scope, $state, $rootScope, $ionicLoading) {
+    $scope.user = {
+        username: null,
+        password: null
+    };
+
+    $scope.error = {};
+
+    $scope.login = function() {
+        $scope.loading = $ionicLoading.show({
+            content: 'Logging in',
+            animation: 'fade-in',
+            showBackdrop: true,
+            maxWidth: 200,
+            showDelay: 0
+        });
+
+        var user = $scope.user;
+        Parse.User.logIn(('' + user.username).toLowerCase(), user.password, {
+            success: function(user) {
+                $ionicLoading.hide();
+                $rootScope.user = user;
+                $rootScope.isLoggedIn = true;
+                $state.go('app.home', {
+                    clear: true
+                });
+            },
+            error: function(user, err) {
+                $ionicLoading.hide();
+                // The login failed. Check error to see why.
+                if (err.code === 101) {
+                    $scope.error.message = 'Invalid login credentials';
+                } else {
+                    $scope.error.message = 'An unexpected error has ' +
+                        'occurred, please try again.';
+                }
+                $scope.$apply();
+            }
+        });
+    };
+
+    $scope.forgot = function() {
+        $state.go('app.forgot');
+    };
+})
+
+.controller('ForgotPasswordController', function($scope, $state, $ionicLoading) {
+    $scope.user = {};
+    $scope.error = {};
+    $scope.state = {
+        success: false
+    };
+
+    $scope.reset = function() {
+        $scope.loading = $ionicLoading.show({
+            content: 'Sending',
+            animation: 'fade-in',
+            showBackdrop: true,
+            maxWidth: 200,
+            showDelay: 0
+        });
+
+        Parse.User.requestPasswordReset($scope.user.email, {
+            success: function() {
+                // TODO: show success
+                $ionicLoading.hide();
+                $scope.state.success = true;
+                $scope.$apply();
+            },
+            error: function(err) {
+                $ionicLoading.hide();
+                if (err.code === 125) {
+                    $scope.error.message = 'Email address does not exist';
+                } else {
+                    $scope.error.message = 'An unknown error has occurred, ' +
+                        'please try again';
+                }
+                $scope.$apply();
+            }
+        });
+    };
+
+    $scope.login = function() {
+        $state.go('app.login');
+    };
+})
+
+.controller('RegisterController', function($scope, $state, $ionicLoading, $rootScope) {
+    $scope.user = {};
+    $scope.error = {};
+
+    $scope.register = function() {
+
+        // TODO: add age verification step
+
+        $scope.loading = $ionicLoading.show({
+            content: 'Sending',
+            animation: 'fade-in',
+            showBackdrop: true,
+            maxWidth: 200,
+            showDelay: 0
+        });
+
+        var user = new Parse.User();
+        user.set("username", $scope.user.email);
+        user.set("password", $scope.user.password);
+        user.set("email", $scope.user.email);
+
+        user.signUp(null, {
+            success: function(user) {
+                $ionicLoading.hide();
+                $rootScope.user = user;
+                $rootScope.isLoggedIn = true;
+                $state.go('app.home', {
+                    clear: true
+                });
+            },
+            error: function(user, error) {
+                $ionicLoading.hide();
+                if (error.code === 125) {
+                    $scope.error.message = 'Please specify a valid email ' +
+                        'address';
+                } else if (error.code === 202) {
+                    $scope.error.message = 'The email address is already ' +
+                        'registered';
+                } else {
+                    $scope.error.message = error.message;
+                }
+                $scope.$apply();
+            }
+        });
+    };
+})
+
+.controller('MainController', function($scope, $state, $rootScope, $stateParams, $ionicHistory) {
+    if ($stateParams.clear) {
+        $ionicHistory.clearHistory();
+    }
+
+    $scope.rightButtons = [{
+        type: 'button-positive',
+        content: '<i class="icon ion-navicon"></i>',
+        tap: function(e) {
+            $scope.sideMenuController.toggleRight();
+        }
+    }];
+
+    $scope.logout = function() {
+        Parse.User.logOut();
+        $rootScope.user = null;
+        $rootScope.isLoggedIn = false;
+        $state.go('welcome', {
+            clear: true
+        });
+    };
+
+    $scope.toggleMenu = function() {
+        $scope.sideMenuController.toggleRight();
+    };
 });
